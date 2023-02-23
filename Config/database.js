a0b24d41a204335cb2b2b3cfc0e0d77a8735830c
Dhanush Saji <dhanushsaji1@gmail.com>
2023-02-23 14:32:16 +0530

    mock-interview-10

---
+++ Config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv').config()
+const MONGO = process.env.MONGO
+const connection = mongoose.connect(`${MONGO}`); //add your password and collection name. Here collection name is 'myfirstdb' (should be smaller in case, simple and only after '.net/')
+
+module.exports = {
+  connection
+};
